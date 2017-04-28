2dca4a07c32a496f66c08bdf9fd6eee3564fba78
Whitney <whitney.purdum@gmail.com>
2017-04-28 14:12:58 -0400

    update favorites

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -14,14 +14,14 @@ class ProgramsController < ApplicationController
   end
 
   def add_favorite
-  @program = Program.find(params[:id])
+  @program = Program.find(params[:program_id])
   @program.favorites.create(user: current_user)
   redirect_to :back
 
   end
 
   def remove_favorite
-  @program = Program.find(params[:id])
+  @program = Program.find(params[:program_id])
   @favorite = Favorite.find_by(program: @program, user: current_user)
   @favorite.destory
   redirect_to :back
