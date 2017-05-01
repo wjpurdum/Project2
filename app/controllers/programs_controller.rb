572a2f90fd4fbfcc15f6e5daafa0c9a141adfee5
Whitney <whitney.purdum@gmail.com>
2017-05-01 11:16:03 -0400

    update

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -28,4 +28,24 @@ class ProgramsController < ApplicationController
   redirect_to :back
   end
 
+  def new
+  @station = Station.new
+  @state = State.find(params[:state_id])
+  end
+
+  def create
+  @state = State.find(params[:state_id])
+  @station = @state.stations.create(station_params)
+  redirect_to state_path(@state)
+  end
+
+  private
+  def station_params
+  params.require(:station).permit(:acronym, :city, :broadcast_area, :station)
+  end
+
+
+
+
+
 end
